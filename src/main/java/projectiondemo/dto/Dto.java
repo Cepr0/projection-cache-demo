56c93881523153895eefd4dc1aeefc1fa24546e6
Cepr0 <cepr0@ukr.net>
2017-06-23 12:44:27 +0300

    return  (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];

--- src/main/java/projectiondemo/dto/Dto.java
+++ src/main/java/projectiondemo/dto/Dto.java
@@ -4,12 +4,17 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.hateoas.Identifiable;
 
 import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
 
 /**
  * @author Cepro, 2017-04-17
  */
 public interface Dto<T, ID extends Serializable> extends Identifiable<ID> {
     
+    @SuppressWarnings("unchecked")
     @JsonIgnore
-    Class<T> getBaseEntity();
+    default Class<T> getBaseEntity() {
+        return  (Class<T>) ((ParameterizedType) getClass()
+                .getGenericSuperclass()).getActualTypeArguments()[0];
+    }
 }
